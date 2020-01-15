1202dab1101cb14215c43b491b76e3d24936eca3
wang <wang@qq.com>
2020-01-15 23:12:12 +0800

    resource

--- src/main/java/com/sukaiyi/visualgit/webhandler/TestHandler.java
+++ src/main/java/com/sukaiyi/visualgit/webhandler/TestHandler.java
@@ -4,20 +4,23 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class TestHandler implements HttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         String requestMethod = httpExchange.getRequestMethod();
         if (requestMethod.equalsIgnoreCase("GET")) {
             Headers responseHeaders = httpExchange.getResponseHeaders();
-            responseHeaders.set("Content-Type", "application/json");
             httpExchange.sendResponseHeaders(200, 0);
 
             OutputStream responseBody = httpExchange.getResponseBody();
-            responseBody.write("response.getBytes()".getBytes());
-
+            InputStream is = getClass().getClassLoader().getResourceAsStream("index.html");
+            BufferedInputStream bis = new BufferedInputStream(is);
+            byte[] buffer = new byte[1024];
+            int count = 0;
+            while ((count = bis.read(buffer)) != -1) {
+                responseBody.write(buffer, 0, count);
+            }
             responseBody.close();
         }
     }
