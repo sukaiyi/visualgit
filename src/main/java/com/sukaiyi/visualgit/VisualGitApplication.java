29826b2057b1eb2aa4c8790491817b2448f031ad
sukaiyi <1433855681@qq.com>
2020-01-16 16:55:46 +0800

    web服务器切换到undertow

--- src/main/java/com/sukaiyi/visualgit/VisualGitApplication.java
+++ src/main/java/com/sukaiyi/visualgit/VisualGitApplication.java
@@ -1,19 +1,18 @@
 package com.sukaiyi.visualgit;
 
-import com.sukaiyi.visualgit.webhandler.TestHandler;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.spi.HttpServerProvider;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
+import io.undertow.Undertow;
+import io.undertow.util.Headers;
 
 public class VisualGitApplication {
 
-    public static void main(String[] args) throws IOException {
-        HttpServerProvider provider = HttpServerProvider.provider();
-        HttpServer httpserver =provider.createHttpServer(new InetSocketAddress(62435), 100);
-        httpserver.createContext("/RestSample", new TestHandler());
-        httpserver.setExecutor(null);
-        httpserver.start();
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(exchange -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseSender().send("Hello World");
+                }).build();
+        server.start();
     }
 }
--- src/main/java/com/sukaiyi/visualgit/webhandler/TestHandler.java
+++ src/main/java/com/sukaiyi/visualgit/webhandler/TestHandler.java
@@ -1,16 +1,17 @@
 package com.sukaiyi.visualgit.webhandler;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class TestHandler implements HttpHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         String requestMethod = httpExchange.getRequestMethod();
         if (requestMethod.equalsIgnoreCase("GET")) {
-            Headers responseHeaders = httpExchange.getResponseHeaders();
             httpExchange.sendResponseHeaders(200, 0);
 
             OutputStream responseBody = httpExchange.getResponseBody();
